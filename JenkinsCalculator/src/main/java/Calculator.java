34872527f69e9b11e50c8c259768b3201f693614
jaes <jpark9509@gmail.com>
2021-02-18 12:07:04 +0900

    Fix Calculator3

--- JenkinsCalculator/src/main/java/Calculator.java
+++ JenkinsCalculator/src/main/java/Calculator.java
@@ -49,13 +49,13 @@ class Calculator {
         if (n==2) {
             return 1;
         }
-        int f = 1;
+        int first = 1;
         int second = 1;
         int res = 0;
 
         for (int i = 3; i <= n; ++i) {
-            res = f + second;
-            f = second;
+            res = first + second;
+            first = second;
             second = res;
         }
 
